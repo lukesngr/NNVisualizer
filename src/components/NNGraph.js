b3decf658fe04bfaf00908e96cf686f356ac9abc
Luke Sanger <lukesngr@gmail.com>
2023-06-12 21:59:11 +1000

    Trying to make UI look nice

--- src/components/NNGraph.js
+++ src/components/NNGraph.js
@@ -2,18 +2,26 @@ function NNGraph(props) {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-2">
                     <h1>Input Layer</h1>
-                    <div className="rounded-circle border border-dark text-center w-50">
+                    <div className="rounded-circle border border-dark text-center">
                         <h1>{props.x1}</h1>
                     </div>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-2">
+                    <div className="text-left">
+                        <p>{props.w1}*{props.x1}</p>
+                        <hr></hr>
+                    </div> 
+                </div>
+                <div className="col-md-2">
                     <h1>Hidden Layer</h1>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
                     <h1>Output Layer</h1>
                 </div>
+                <div className="col-md-2"></div>
             </div>
         </div>
     )
--- src/components/NNVisualizer.js
+++ src/components/NNVisualizer.js
@@ -9,7 +9,7 @@ function NNVisualizer() {
     const [biasTwo, setBiasTwo] = useState(1);
     return (<>
         <NNGraph x1={xOne} w1={weightOne} b1={biasOne} w2={weightTwo} b2={biasTwo}></NNGraph>
-        <div className='text-center'>
+        <div className='text-center mt-5'>
             x1: <input type="text" value={xOne} onChange={(event) => setXOne(event.target.value)}></input>,
             w1: <input type="text" value={weightOne} onChange={(event) => setWeightOne(event.target.value)}></input>,
             b1: <input type="text" value={biasOne} onChange={(event) => setBiasOne(event.target.value)}></input>,
