4cdeebfc365082456d76dd5f90377b7e8472c563
Luke Sanger <lukesngr@gmail.com>
2023-06-12 22:26:18 +1000

    Made returnValueOfPerceptron more solid

--- src/components/NNGraph.js
+++ src/components/NNGraph.js
@@ -1,3 +1,5 @@
+import returnValueOfPerceptron from "../returnValueOfPerceptron";
+
 function NNGraph(props) {
     return (
         <div className="container">
@@ -9,17 +11,23 @@ function NNGraph(props) {
                     </div>
                 </div>
                 <div className="col-md-2" style={{paddingLeft: 0}}>
-                    <div style={{marginTop: "50%"}} className="text-left">
+                    <div style={{marginTop: "20%"}} className="text-left">
                         <p>w1*x1 = {props.w1}*{props.x1}</p>
                         <hr></hr>
                     </div> 
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-2" style={{paddingLeft: 0, paddingRight: 0}}>
                     <h1>Hidden Layer</h1>
+                    <div className="rounded-circle border border-dark text-center p-1">
+                        <h3>S({props.x1*props.w1}+{props.b1}) = {returnValueOfPerceptron(props.x1*props.w1, props.b1).toFixed(2)}</h3>
+                    </div>
                 </div>
                 <div className="col-md-2"></div>
                 <div className="col-md-2">
                     <h1>Output Layer</h1>
+                    <div className="rounded-circle border border-dark text-center">
+                        <h1>{props.x1}</h1>
+                    </div>
                 </div>
                 <div className="col-md-2"></div>
             </div>
--- src/returnValueOfPerceptron.js
+++ src/returnValueOfPerceptron.js
@@ -1,7 +1,8 @@
 /*function does not perform weighted multiplication because despite only having one neuron in this vizualizer 
 I do not want to give off the impression that the bias is applied to every weighted input but rather to the sum of the weighted inputs*/
 function returnValueOfPerceptron(sumOfWeightedValues, bias) {
-    let denominator = 1+Math.pow(Math.E, -(sumOfWeightedValues+bias));
+    let sum = parseFloat(sumOfWeightedValues)+parseFloat(bias);
+    let denominator = 1+Math.pow(Math.E, -sum);
     return 1/denominator;
 }
 
