92c46494521a63184c9830dc2b388917242b9bd9
Luke Sanger <lukesngr@gmail.com>
2023-06-12 21:45:57 +1000

    Building first node

--- src/components/NNGraph.js
+++ src/components/NNGraph.js
@@ -1,5 +1,22 @@
 function NNGraph(props) {
-    return <p>Graph</p>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <h1>Input Layer</h1>
+                    <div className="rounded-circle border border-dark text-center w-50">
+                        <h1>{props.x1}</h1>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <h1>Hidden Layer</h1>
+                </div>
+                <div className="col-md-4">
+                    <h1>Output Layer</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NNGraph;
--- src/components/NNVisualizer.js
+++ src/components/NNVisualizer.js
@@ -9,12 +9,12 @@ function NNVisualizer() {
     const [biasTwo, setBiasTwo] = useState(1);
     return (<>
         <NNGraph x1={xOne} w1={weightOne} b1={biasOne} w2={weightTwo} b2={biasTwo}></NNGraph>
-        <div>
+        <div className='text-center'>
             x1: <input type="text" value={xOne} onChange={(event) => setXOne(event.target.value)}></input>,
             w1: <input type="text" value={weightOne} onChange={(event) => setWeightOne(event.target.value)}></input>,
             b1: <input type="text" value={biasOne} onChange={(event) => setBiasOne(event.target.value)}></input>,
             w2: <input type="text" value={weightTwo} onChange={(event) => setWeightTwo(event.target.value)}></input>,
-            b2: <input type="text" value={biasTwo} onChange={(event) => setBiasTwo(event.target.value)}></input>,
+            b2: <input type="text" value={biasTwo} onChange={(event) => setBiasTwo(event.target.value)}></input>
         </div>
         </>
     )
