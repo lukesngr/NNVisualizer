ee182582c486da2be717263d71ef3305ce67a921
Luke Sanger <lukesngr@gmail.com>
2023-06-12 22:45:25 +1000

    NN representation complete, trying to modularize

--- src/components/NNGraph.js
+++ src/components/NNGraph.js
@@ -1,17 +1,22 @@
 import returnValueOfPerceptron from "../returnValueOfPerceptron";
 
 function NNGraph(props) {
+
+    var hiddenParameter = props.x1*props.w1+"+"+props.b1;
+    var hiddenResult = returnValueOfPerceptron(props.x1*props.w1, props.b1);
+    var outputParameter = (props.w2*hiddenResult).toFixed(2)+"+"+props.b2;
+    var outputResult = returnValueOfPerceptron(hiddenResult*props.w2, props.b2).toFixed(2);
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-2" style={{paddingRight: 0}}>
                     <h1>Input Layer</h1>
-                    <div className="rounded-circle border border-dark text-center">
+                    <div className="rounded-circle border border-dark text-center mt-5">
                         <h1>{props.x1}</h1>
                     </div>
                 </div>
-                <div className="col-md-2" style={{paddingLeft: 0}}>
-                    <div style={{marginTop: "20%"}} className="text-left">
+                <div className="col-md-2" style={{paddingLeft: 0, paddingRight: 0}}>
+                    <div style={{marginTop: "40%"}} className="text-left">
                         <p>w1*x1 = {props.w1}*{props.x1}</p>
                         <hr></hr>
                     </div> 
@@ -19,14 +24,19 @@ function NNGraph(props) {
                 <div className="col-md-2" style={{paddingLeft: 0, paddingRight: 0}}>
                     <h1>Hidden Layer</h1>
                     <div className="rounded-circle border border-dark text-center p-1">
-                        <h3>S({props.x1*props.w1}+{props.b1}) = {returnValueOfPerceptron(props.x1*props.w1, props.b1).toFixed(2)}</h3>
+                        <h3>S({hiddenParameter}) = {hiddenResult.toFixed(2)}</h3>
                     </div>
                 </div>
-                <div className="col-md-2"></div>
-                <div className="col-md-2">
+                <div className="col-md-2"  style={{paddingLeft: 0, paddingRight: 0}}>
+                    <div style={{marginTop: "40%"}} className="text-left">
+                        <p>w2*input = {props.w2}*{returnValueOfPerceptron(props.x1*props.w1, props.b1).toFixed(2)}</p>
+                        <hr></hr>
+                    </div> 
+                </div>
+                <div className="col-md-2"  style={{paddingLeft: 0, paddingRight: 0}}>
                     <h1>Output Layer</h1>
                     <div className="rounded-circle border border-dark text-center">
-                        <h1>{props.x1}</h1>
+                        <h3>S({outputParameter}) = {outputResult}</h3>
                     </div>
                 </div>
                 <div className="col-md-2"></div>
