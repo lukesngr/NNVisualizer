072f526a4461331bf89ad7981732fad085aa2721
Luke Sanger <lukesngr@gmail.com>
2023-06-12 19:11:42 +1000

    Wasted so much time when I could of used simple CSS

--- src/Intro.js
+++ src/Intro.js
@@ -1,8 +1,10 @@
 function Intro() {
     return <>
-        <h1>Simple Neural Network Math Visualizer</h1>
-        <p>When I started learning neural networks, I had trouble understanding the math so I made these examples</p>
-        <b>Note: one neuron for each layer for ease of visualization</b>
+        <h1 className="text-center">Simple Neural Network Math Visualizer</h1>
+        <div className="centeredDescription">
+            <p>When I started learning neural networks, I had trouble understanding the math so I made these examples <br></br>
+            <b>Note: one neuron for each layer for ease of visualization</b></p>
+        </div>
     </>
 }
 
