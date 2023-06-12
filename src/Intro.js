1a566a29ef49af3d0e55132d198c911f6d213e54
Luke Sanger <lukesngr@gmail.com>
2023-06-12 18:35:22 +1000

    TDD already going well

---
+++ src/Intro.js
@@ -0,0 +1,9 @@
+function Intro() {
+    return <>
+        <h1>Simple Neural Network Math Visualizer</h1>
+        <p>When I started learning neural networks, I had trouble understanding the math so I made these examples</p>
+        <b>Note: one neuron for each layer for ease of visualization</b>
+    </>
+}
+
+export default Intro;
